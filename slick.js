665f256de9a12f2cd23704e979b924889b25621c
Sunjid Rayhan <73388921+SunjidRayhan@users.noreply.github.com>
2022-06-15 15:31:19 +0600

    Add files via upload

---
+++ slick.js
@@ -0,0 +1,39 @@
+$('.team-slider').slick({
+    dots: false,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout:1000,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
